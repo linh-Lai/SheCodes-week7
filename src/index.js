6b3c7eafc9e47d254d93574e04f9f399149d2d72
linh-Lai <92214613+linh-Lai@users.noreply.github.com>
2021-10-16 21:03:02 +0200

    updated icon element

--- src/index.js
+++ src/index.js
@@ -17,24 +17,30 @@ function formatDate(timestamp) {
 
 
 function displayTemperature(response) {
-   //console.log(response.data.main.humidity)
+  console.log(response.data)
     let location = document.querySelector("#city")
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     location.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity + "%";
     windElement.innerHTML = Math.round(response.data.wind.speed) + "km/h";
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-   
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute(
+        "alt", response.data.weather[0].description);
 }
 
 let apiKey = "d6ff37a4c170f249f41b8f3e7f2c9d0a";
-let apiUrl = `https:/api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&appid=${apiKey}`;
+let city = "Berlin";
+let apiUrl = `https:/api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature)
