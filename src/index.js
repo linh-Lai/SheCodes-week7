606ebcd6da516267668db95b23e4eb3a4e937db0
linh-Lai <92214613+linh-Lai@users.noreply.github.com>
2021-10-17 22:00:20 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ function formatDate(timestamp) {
     if (minutes < 10) {
         minutes =`0${minutes}`;
     }
-    
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}` ;
